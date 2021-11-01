e9f95589a7e7a47dcb4c4459ccc195e4f9c8a768
kolop <kolop@SHANDOR>
2021-11-01 18:40:40 +0300

    Добавьте файлы проекта.

---
+++ Project_14_3_3/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_14_3_3
+{
+    internal class Contact
+    {
+        public Contact(string name, string lastName, long phoneNumber, string email)
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        public long PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+    }
+}
---
+++ Project_14_3_3/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Project_14_3_3;
+
+List<Contact> phoneBook = new List<Contact>();
+phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+var sortedBook = phoneBook.OrderBy(s => s.Name).ThenBy(s => s.LastName);
+
+foreach (var contact in sortedBook)
+{
+    Console.WriteLine(
+        "Имя : {0} {1} | Тел. Номер : {2} | Почта : {3}", 
+        contact.Name, 
+        contact.LastName, 
+        contact.PhoneNumber,
+        contact.Email
+        );
+}
+
+Console.ReadKey();
